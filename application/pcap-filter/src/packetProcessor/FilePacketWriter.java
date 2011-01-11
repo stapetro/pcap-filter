7097c21cf9c619531092337e3fde5d7efa2664a1
kbailov <kbailov@7c1a397a-d933-ff38-ee66-82a4fbc0a7f4>
2011-01-11 21:36:40 +0000

    Implement factories and functionality regarding writing packets

--- application/pcap-filter/src/packetProcessor/FilePacketWriter.java
+++ application/pcap-filter/src/packetProcessor/FilePacketWriter.java
@@ -1,13 +1,43 @@
 package packetProcessor;
 
+import jpcap.JpcapWriter;
 import jpcap.packet.Packet;
 
-public class FilePacketWriter implements IPacketWriter{
+/**
+ * This class writes packets to files.
+ */
+public class FilePacketWriter implements IPacketWriter {
 
+	/**
+	 * File packet writer
+	 */
+	private JpcapWriter writer;
+
+	/**
+	 * Constructor
+	 * 
+	 * @param output
+	 *            the writer used to write packets to file
+	 */
+	public FilePacketWriter(JpcapWriter output) {
+		writer = output;
+	}
+
+	/**
+	 * Write the specified packet to file
+	 */
 	@Override
 	public void writePacket(Packet packet) {
-		// TODO Auto-generated method stub
-		
+		writer.writePacket(packet);
+
+	}
+
+	/**
+	 * Close file
+	 */
+	@Override
+	public void close() {
+		writer.close();
 	}
 
 }
--- application/pcap-filter/src/packetProcessor/NetworkInterfacePacketWriter.java
+++ application/pcap-filter/src/packetProcessor/NetworkInterfacePacketWriter.java
@@ -1,12 +1,39 @@
 package packetProcessor;
 
+import jpcap.JpcapSender;
 import jpcap.packet.Packet;
 
-public class NetworkInterfacePacketWriter implements IPacketWriter{
+/**
+ * This class writes packets through network interfaces
+ */
+public class NetworkInterfacePacketWriter implements IPacketWriter {
 
+	/**
+	 * Network interface packet writer
+	 */
+	private JpcapSender sender;
+
+	/**
+	 * Constructor
+	 * 
+	 * @param output
+	 *            the sender used to send packets througn network interface
+	 */
+	public NetworkInterfacePacketWriter(JpcapSender output) {
+		sender = output;
+	}
+
+	/**
+	 * Close sending network interface
+	 */
 	@Override
 	public void writePacket(Packet packet) {
-		// TODO Auto-generated method stub
+		sender.sendPacket(packet);
 	}
-	
+
+	@Override
+	public void close() {
+		sender.close();
+	}
+
 }
