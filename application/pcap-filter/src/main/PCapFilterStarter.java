903fc9b9b8068b06493a4af8576fe7be424375d1
kbailov <kbailov@7c1a397a-d933-ff38-ee66-82a4fbc0a7f4>
2011-01-12 21:29:00 +0000

    Implement packet modification and provide configuration file for modifying packets

--- application/pcap-filter/src/main/PCapFilterStarter.java
+++ application/pcap-filter/src/main/PCapFilterStarter.java
@@ -1,18 +1,19 @@
 package main;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.List;
+import java.util.Properties;
 import java.util.Scanner;
 
+import manipulator.SipManager;
+
 import jpcap.JpcapCaptor;
-import jpcap.JpcapWriter;
 import jpcap.NetworkInterface;
 import jpcap.packet.Packet;
 import packetProcessor.IPacketReader;
-import packetProcessor.IPacketWriter;
-import packetProcessor.NetworkInterfacePacketReader;
 import packetProcessor.PacketReaderFactory;
-import packetProcessor.PacketWriterFactory;
 import constants.PCapFilterConstants;
 
 /**
@@ -26,46 +27,81 @@ public class PCapFilterStarter {
 	 */
 	public static void main(String[] args) {
 		/**** Stanislav Petrov Code *****/
-
-		String outputSipFile = "sip_session_captured.jpcap";
-		// String outputSipFile = "test_packets";
-		int deviceIndex = 1;
-
-		// Obtain the list of network interfaces
-		NetworkInterface[] devices = JpcapCaptor.getDeviceList();
-		NetworkInterface wanInterface = devices[deviceIndex];
+		//
+		// String outputSipFile = "sip_session_captured.jpcap";
+		// // String outputSipFile = "test_packets";
+		// int deviceIndex = 1;
+		//
+		// // Obtain the list of network interfaces
+		// NetworkInterface[] devices = JpcapCaptor.getDeviceList();
+		// NetworkInterface wanInterface = devices[deviceIndex];
+		// // IPacketReader interfacePacketReader = PacketReaderFactory
+		// // .getPacketReader(wanInterface, PCapFilterConstants.SIP_FILTER);
 		// IPacketReader interfacePacketReader = PacketReaderFactory
-		// .getPacketReader(wanInterface, PCapFilterConstants.SIP_FILTER);
+		// .getPacketReader(outputSipFile, PCapFilterConstants.SIP_FILTER);
+		//
+		// System.out.println("Start listening for packets...");
+		// interfacePacketReader.startReadingPackets();
+		// System.out.println("Finish listening for packets...");
+		// JpcapCaptor captor = interfacePacketReader.getCaptor();
+		// IPacketWriter packetWriter = null;
+		// try {
+		// // JpcapWriter writer = JpcapWriter
+		// // .openDumpFile(captor, outputSipFile);
+		// // packetWriter = PacketWriterFactory.getPacketWriter(writer);
+		// // Reading of packets shouldn't be here.
+		// List<Packet> packets = interfacePacketReader.getPackets();
+		// if (packets != null) {
+		// for (Packet currPacket : packets) {
+		// // packetWriter.writePacket(currPacket);
+		// // System.out.println(currPacket);
+		// }
+		// }
+		// // } catch (IOException e) {
+		// // e.printStackTrace();
+		// } finally {
+		// if (packetWriter != null) {
+		// packetWriter.close();
+		// }
+		// }
+		// interfacePacketReader.close();
+
+		/**** Krasimir Baylov Code *****/
+		String outputSipFile = "sip_session_captured.jpcap";
+
 		IPacketReader interfacePacketReader = PacketReaderFactory
 				.getPacketReader(outputSipFile, PCapFilterConstants.SIP_FILTER);
 
-		System.out.println("Start listening for packets...");
+		//System.out.println("Start listening for packets...");
 		interfacePacketReader.startReadingPackets();
-		System.out.println("Finish listening for packets...");
-		JpcapCaptor captor = interfacePacketReader.getCaptor();
-		IPacketWriter packetWriter = null;
+		//System.out.println("Finish listening for packets...");
 		try {
-			// JpcapWriter writer = JpcapWriter
-			// .openDumpFile(captor, outputSipFile);
-			// packetWriter = PacketWriterFactory.getPacketWriter(writer);
-			// Reading of packets shouldn't be here.
 			List<Packet> packets = interfacePacketReader.getPackets();
+			
+			Properties prop = new Properties();
+			prop.load(new FileInputStream("SIP_MASK.properties"));
+
+			SipManager sipManager = new SipManager(prop);
+			
 			if (packets != null) {
 				for (Packet currPacket : packets) {
-					// packetWriter.writePacket(currPacket);
-					// System.out.println(currPacket);
+					System.out.println("-----\n old:");
+					System.out.print(new String(currPacket.data));
+					byte[] modifiedSipPacket = sipManager.modifyPacket(currPacket.data);
+					
+					System.out.println("-----\n new: ");
+					
+					System.out.print(new String(modifiedSipPacket));
+					System.out.println("-----");
 				}
 			}
-			// } catch (IOException e) {
-			// e.printStackTrace();
-		} finally {
-			if (packetWriter != null) {
-				packetWriter.close();
-			}
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 		interfacePacketReader.close();
-
-		/**** Krasimir Baylov Code *****/
 
 	}
 
@@ -88,8 +124,8 @@ public class PCapFilterStarter {
 			try {
 				deviceNumber = Integer.parseInt(line);
 				index = deviceNumber - 1;
-				if(index >= 0 && index < devices.length) {
-					break;					
+				if (index >= 0 && index < devices.length) {
+					break;
 				} else {
 					System.out.println("Number is out of range");
 				}
---
+++ application/pcap-filter/src/manipulator/SipManager.java
@@ -0,0 +1,75 @@
+package manipulator;
+
+import java.util.Properties;
+
+/**
+ * This class is responsible for manipulating packets. The headers that need to
+ * be manipulated are defined in the properties file that goes in the
+ * constructor.
+ */
+public class SipManager {
+
+	/**
+	 * String separating the header from its value.
+	 */
+	private static final String HEADER_VALUE_SEPARATOR = ":";
+
+	/**
+	 * Properties file that defines the rules that packets are modified
+	 */
+	private Properties rules;
+
+	/**
+	 * Constructor
+	 * 
+	 * @param prop
+	 *            properties file defining the rules for manipulating packets
+	 */
+	public SipManager(Properties prop) {
+		rules = prop;
+	}
+
+	/**
+	 * Modify the provided packet
+	 * 
+	 * @param packet
+	 *            packet that need to be modified
+	 * @return the modified packet. Modification is based on the properties file
+	 *         that the class was initialized with
+	 */
+	public byte[] modifyPacket(byte[] packet) {
+		String sipPacketString = new String(packet);
+		String[] sipElements = sipPacketString.split("\\n");
+
+		for (int i = 0; i < sipElements.length; i++) {
+			for (Object key : rules.keySet()) {
+				String headerName = (String) key;
+
+				if (sipElements[i].startsWith(headerName)) {
+					String newHeaderData = rules.getProperty(headerName);
+					sipElements[i] = rewriteElement(headerName, newHeaderData);
+				}
+			}
+		}
+
+		StringBuilder newSipPacketSb = new StringBuilder();
+		for (int i = 0; i < sipElements.length - 1; i++) {
+			newSipPacketSb.append(sipElements[i]);
+			newSipPacketSb.append("\n");
+		}
+		// omit the new line to keen the initial format
+		newSipPacketSb.append(sipElements[sipElements.length - 1]);
+
+		return newSipPacketSb.toString().getBytes();
+	}
+
+	private String rewriteElement(String headerName, String newValue) {
+		String newElement = new String();
+		newElement += headerName;
+		newElement += HEADER_VALUE_SEPARATOR;
+		newElement += " ";
+		newElement += newValue;
+
+		return newElement;
+	}
+}
