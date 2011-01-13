63e4db753c9356c5d48174b7f2de3b1b8f8df1cc
devilfighter1806 <devilfighter1806@7c1a397a-d933-ff38-ee66-82a4fbc0a7f4>
2011-01-13 23:05:24 +0000

    Analyze TCP and UDP traffic. To be categorized sessions by transport protocol (UDP, TCP).

--- trunk/application/pcap-filter/src/packetProcessor/PacketAnalyzer.java
+++ trunk/application/pcap-filter/src/packetProcessor/PacketAnalyzer.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import jpcap.PacketReceiver;
 import jpcap.packet.Packet;
+import jpcap.packet.TCPPacket;
 import jpcap.packet.UDPPacket;
 
 public class PacketAnalyzer implements PacketReceiver {
@@ -32,11 +33,21 @@ public class PacketAnalyzer implements PacketReceiver {
 		numberOfCapturedPackets++;
 		// analyze packet
 		if (packet instanceof UDPPacket) {
+			// System.out.println(packet);
 			UDPPacket udpPacket = (UDPPacket) packet;
 			InetAddress sourceIPAddr = udpPacket.src_ip;
 			InetAddress destIPAddr = udpPacket.dst_ip;
 			int sourcePort = udpPacket.src_port;
 			int destPort = udpPacket.dst_port;
+			boolean result = isNetworkSessionExists(sourceIPAddr, destIPAddr,
+					sourcePort, destPort);
+		} else 	if (packet instanceof TCPPacket) {
+			// System.out.println(packet);
+			TCPPacket tcpPacket = (TCPPacket)packet;
+			InetAddress sourceIPAddr = tcpPacket.src_ip;
+			InetAddress destIPAddr = tcpPacket.dst_ip;
+			int sourcePort = tcpPacket.src_port;
+			int destPort = tcpPacket.dst_port;
 			boolean result = isNetworkSessionExists(sourceIPAddr, destIPAddr,
 					sourcePort, destPort);
 		}
@@ -63,13 +74,15 @@ public class PacketAnalyzer implements PacketReceiver {
 				destIPAddr, sourcePort, destPort);
 		if (networkSessions.size() > 0) {
 			for (NetworkSession session : networkSessions) {
+//				System.out.println("Other: " + otherSession + "\nCurrent: "
+//						+ session);
 				boolean result = otherSession.equals(session);
-				System.out.println("Res: " + result);
 				if (otherSession.equals(session)) {
 					session.incrementNumberOfPackets();
 					return true;
 				}
 			}
+			networkSessions.add(otherSession);
 		} else {
 			networkSessions.add(otherSession);
 			otherSession.incrementNumberOfPackets();
